500236c0493dd834a568ca1f7b5e6377195cd63c
takada hideaki <takadahideaki007@gmail.com>
2021-01-21 18:37:00 +0900

    Initial Commit

---
+++ Asynchronous Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Asynchronous Practice
+//
+//  Created by takadahideaki007 on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
